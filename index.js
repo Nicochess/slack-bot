65adb98ee316133eaaef979264fa4dee4fdb31cc
Nicolas Magno <66505477+Nicochess@users.noreply.github.com>
2022-01-21 15:09:04 -0300

    feat: user could request from slack state of section asana

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ require("dotenv").config();
 
 const { App } = require("@slack/bolt");
 
+const { Client } = require("asana");
+
+const client = Client.create().useAccessToken(process.env.ACESS_TOKEN);
+
 const app = new App({
   token: process.env.BOT_TOKEN,
   appToken: process.env.SLACK_TOKEN,
@@ -10,9 +14,56 @@ const app = new App({
   socketMode: true,
 });
 
-app.message('bot', async ({ message, say }) => {
-    await say(`Por que está falando de mim, <@${message.user}> ?`);
-  });
+app.message("bot", async ({ message, say }) => {
+  await say(`Por que está falando de mim, <@${message.user}> ?`);
+});
+
+app.message("task doing", async ({ say }) => {
+  try {
+    const { data } = await client.tasks.getTasksForSection("1140999922606648", {
+      completed_since: "now",
+    });
+
+    if (data.length == 0) {
+      say("Ninguém trabalhando? :eyes:");
+      return;
+    }
+
+    let message = "";
+    data.map((task) => (message += `- ${task.name} \n`));
+    say(message);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.message("task todo", async ({ say }) => {
+  try {
+    const { data } = await client.tasks.getTasksForSection("1140999922812186", {
+      completed_since: "now",
+    });
+
+    if (data.length == 0) {
+      say("Nenhuma tarefa pendente. :eyes:");
+      return;
+    }
+
+    let message = "";
+    res.data.map((task) => (message += `- ${task.name} \n`));
+    say(message);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.message("comandos", async ({ say, message }) => {
+  say(`
+    Olá, <@${message.user}> !
+    Nós podemos nos comunicar utilizando os comandos:
+    - task doing
+    - task todo
+  `);
+});
 
 (async () => {
   await app.start();
